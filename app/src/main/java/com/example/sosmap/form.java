9234cefc7cbe58d5f9ebb3adf6e178ec7824b396
Vered Sayag <vered8765@gmail.com>
2019-09-25 18:01:46 +0300

    The form active desigin

---
+++ app/src/main/java/com/example/sosmap/form.java
@@ -0,0 +1,32 @@
+package com.example.sosmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+//create a list of items for the spinner.
+        String[] items = new String[]{"First aid kit", "I have basic medical training", "I'm a doctor","defibrillator"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+    }
+    public void SendClick(View view)
+    {
+        Intent intent = new Intent(form.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
