09054269172507990aa7a2b9d90c0cbda153c62c
Elad Israel <elad.israel@live.biu.ac.il>
2019-09-25 14:10:05 +0300

    Map added

--- app/src/main/java/com/example/sosmap/MainActivity.java
+++ app/src/main/java/com/example/sosmap/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sosmap;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void MapClick(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
     }
 }
